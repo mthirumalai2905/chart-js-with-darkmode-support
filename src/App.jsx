bc605a15bbe96b63bc8a4689a82c02319f3e7873
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-02-13 15:09:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
 import './App.css';
 
+
 const App = () => {
   const jsChartRef = useRef(null);
   const pythonChartRef = useRef(null);
